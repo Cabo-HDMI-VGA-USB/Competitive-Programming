e66b22a99a965010c46541d911249ddb63924c27
duduFreire <dudufreiresantos@gmail.com>
2023-08-25 18:07:58 -0300

    improve hash

--- wiki/string/hash.cpp
+++ wiki/string/hash.cpp
@@ -17,13 +17,16 @@ using namespace std;
 
 int m1, m2;
 
+int m1, m2;
+int n; string s;
+
 struct Hash {
     const int P = 31;
     int n; string s;
     vector<int> h, hi, p, p2, h2, hi2;
     Hash() {}
     Hash(string s):
-	s(s), n(s.size()), h(n), hi(n), p(n), h2(n), hi2(n),p2(n) {
+	s(s), n(s.size()), h(n), hi(n), p(n), h2(n), hi2(n), p2(n) {
         for (int i=0;i<n;i++) p[i] = (i ? P*p[i-1]:1) % m1;
         for (int i=0;i<n;i++) p2[i] = (i ? P*p2[i-1]:1) % m2;
 
@@ -44,15 +47,14 @@ struct Hash {
         hash2 = hash2 < 0 ? hash2 + m2 : hash2;
 		return (hash << 30) ^ hash2;
     }
-    int gethashinv(int l, int r) {
+    int gethashi(int l, int r) {
         int hash = (hi[l] - (r+1 < n ? hi[r+1]*p[r-l+1] % m1 : 0));
         int hash2 = (hi2[l] - (r+1 < n ? hi2[r+1]*p2[r-l+1] % m2 : 0));
         hash= hash < 0 ? hash + m1 : hash;
-        hash2= hash2 < 0 ? hash + m2 : hash2;
+        hash2= hash2 < 0 ? hash2 + m2 : hash2;
 		return (hash << 30) ^ hash2;
     }
 };
-
 
 void solve()
 {
